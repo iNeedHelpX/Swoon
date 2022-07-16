0724fce237242e6d27a38db3774229f60ee46871
Julia Pak <jpak0444@gmail.com>
2022-07-16 16:16:28 -0400

    ok

--- lib/auth/google_auth.dart
+++ lib/auth/google_auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:swooncustomerapp/pages/loginpage.dart';
 import 'package:swooncustomerapp/start/app_setup.dart';
 
@@ -18,5 +19,14 @@ class AuthService {
 
   signInWithGoogle() async {
     //stuff
+
+    final GoogleSignInAccount? user =
+        await GoogleSignIn(scopes: <String>["email"]).signIn();
+    final GoogleSignInAuthentication googleAuth = await user!.authentication;
+
+    final cred = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+    return await FirebaseAuth.instance.signInWithCredential(cred);
   }
 }
